1f68693d34c98234714c12ba33ae10ab5aabd7a9
HugoSousa-38 <a46304@alunos.isel.pt>
2022-08-22 22:44:07 +0100

    Social Bubble

--- Brackeys GJ/Assets/Scripts/Player.cs
+++ Brackeys GJ/Assets/Scripts/Player.cs
@@ -9,18 +9,21 @@ public class Player : MonoBehaviour
     public int radius;
     public UIManager manager;
 
+    public Transform socialBubble;
+    private Renderer bubbleMat;
+
     private float noise = 0;
     public float damageRate;
     public float healRate;
     public float speed;
     
-    // Start is called before the first frame update
     void Start()
     {
         manager.SetNoiseLevel(noise);
+        socialBubble.localScale = new Vector3(radius * 2, radius * 2, 1);
+        bubbleMat = socialBubble.gameObject.GetComponent<Renderer>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         CheckEnemy();
@@ -44,6 +47,25 @@ public class Player : MonoBehaviour
         {
             noise = Mathf.Clamp(noise -= Time.deltaTime * healRate,0,100);
             manager.SetNoiseLevel(noise);
+        }
+        
+        bubbleMat.material.SetFloat("_Noise", noise*0.01f);
+        SetRippleRate();
+    }
+
+    private void SetRippleRate()
+    {
+        if (noise <= 20)
+        {
+            bubbleMat.material.SetFloat("_Rate", 0.3f);
+        }
+        if (noise > 20 && noise <= 70)
+        {
+            bubbleMat.material.SetFloat("_Rate", 1f);
+        }
+        if (noise > 70)
+        {
+            bubbleMat.material.SetFloat("_Rate", 2f);
         }
     }
 
